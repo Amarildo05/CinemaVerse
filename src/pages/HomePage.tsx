a1744db9e1b6b5c6d77c82687af68e5d971961eb
Amarildo <amarildokola36@gmail.com>
2024-10-16 01:10:30 +0200

    Adjusting the carousel styles.Adding dark/light mode for backgound and movieCard/ app is looking better now

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -3,10 +3,18 @@ import PopularMovies from "../componenets/HomePage/popularMovies/PopularMovies";
 import AllMovies from "../componenets/HomePage/allMovies/AllMovies";
 import Navbar from "../componenets/common/navbar/Navbar";
 import "./PageContainer.css";
+import { useContext } from "react";
+import { GlobalContext } from "../context/GlobalContext";
 
 export default function HomePage() {
+  const themeContext = useContext(GlobalContext);
   return (
-    <div className="page-container">
+    <div
+      className="page-container"
+      style={{
+        backgroundColor: themeContext.theme === "dark" ? "#5a5a5a " : "#e6e6e6 ",
+      }}
+    >
       <Navbar />
       <PopularMovies />
       <AllMovies />
