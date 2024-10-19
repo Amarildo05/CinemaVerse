9a5d8e9c5d00957bf2cc4a5b061aca32110de53b
Amarildo <amarildokola36@gmail.com>
2024-10-19 13:10:53 +0200

    fixed the navigate for the movie cards to go to SingleMovie(id)/added a filler image when movieCard image comes as null

--- src/componenets/HomePage/movieCart/MovieCart.tsx
+++ src/componenets/HomePage/movieCart/MovieCart.tsx
@@ -3,11 +3,13 @@ import "./MovieCart.css";
 import { AllMoviesType } from "../../../types";
 import { GlobalContext } from "../../../context/GlobalContext";
 import { useContext } from "react";
-import { Link } from "react-router-dom";
+import useMovieNavigation from "../../../hooks/useMovieNavigation";
 
 const { Meta } = Card;
 
 export default function MovieCard(props: AllMoviesType) {
+  const handleNavigateClick = useMovieNavigation(props.id);
+
   const themeContext = useContext(GlobalContext);
 
   const rating = props.vote_average / 2;
@@ -21,44 +23,53 @@ export default function MovieCard(props: AllMoviesType) {
   );
 
   return (
-    <Link to={"/singleMovie"}>
-      <Popover placement="leftTop" content={content}>
-        <Card
-          className="movie-card"
-          style={{
-            backgroundColor:
-              themeContext.theme === "dark" ? "#404040" : "#b8b8b8",
-            border:
-              themeContext.theme === "dark"
-                ? "1px solid #404040"
-                : "1px solid #b8b8b8",
-          }}
-          hoverable
-          cover={
-            <img
-              alt="Movie Image"
-              src={`https://image.tmdb.org/t/p/w500${props.poster_path}`}
-              className="movie-card-image"
-            />
-          }
-        >
-          <Meta
-            className="meta-title white-text"
-            title={
-              <span
-                style={{
-                  color: themeContext.theme === "dark" ? "white" : "black",
-                }}
-              >
-                {props.title}
-              </span>
+    <Popover placement="leftTop" content={content}>
+      <Card
+        onClick={handleNavigateClick}
+        className="movie-card"
+        style={{
+          backgroundColor:
+            themeContext.theme === "dark" ? "#404040" : "#b8b8b8",
+          border:
+            themeContext.theme === "dark"
+              ? "1px solid #404040"
+              : "1px solid #b8b8b8",
+        }}
+        hoverable
+        cover={
+          <img
+            alt={props.title}
+            src={
+              props.poster_path
+                ? `https://image.tmdb.org/t/p/w500${props.poster_path}`
+                : "/filler-image.jpg"
             }
-            // description={
-            //   <span>{props.genre_ids}</span>
-            // }
+            className="movie-card-image"
           />
-        </Card>
-      </Popover>
-    </Link>
+        }
+      >
+        <Meta
+          className="meta-title white-text"
+          title={
+            <span
+              style={{
+                color: themeContext.theme === "dark" ? "white" : "black",
+              }}
+            >
+              {props.title}
+            </span>
+          }
+          // description={
+          //   <span
+          //     style={{
+          //       color: themeContext.theme === "dark" ? "white" : "black",
+          //     }}
+          //   >
+          //     {props.genre_ids}
+          //   </span>
+          // }
+        />
+      </Card>
+    </Popover>
   );
 }
---
+++ src/hooks/useMovieNavigation.ts
@@ -0,0 +1,12 @@
+import { useNavigate } from "react-router-dom";
+
+function useMovieNavigation(id: number) {
+  const nav = useNavigate();
+
+  function handleNavigateClick() {
+    nav(`/singleMovie/${id}`);
+  }
+  return handleNavigateClick;
+}
+
+export default useMovieNavigation;
